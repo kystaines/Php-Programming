8ceaa1e737270ae7deb921a3cf77e3ebe781455b
kystaines <ysa6anda@gmail.com>
2024-05-29 18:57:54 +0800

    Update welcome.php

---
+++ welcome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Programming</title>
+</head>
+<body>
+    <?php 
+        echo 'Welcome to PHP <br/>';
+        echo date('Y-m-d H:i:s'); 
+        echo "<br/> Tomorrow I'll learn PHP global variables. <br/>";
+        echo "This is a bad command : del c:\\*.*";
+    ?>
+</body>
+</html>
